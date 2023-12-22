fc0e00e1370fddf2693ade461bf3107fcb492d90
Platon <xnpltn@gmail.com>
2023-12-23 01:18:51 +0200

    first commit

---
+++ src/add/main.py
@@ -0,0 +1,8 @@
+
+
+def add_1(x):
+    return x + 1
+
+
+def add_2(x):
+    return x + 2
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+from add.main import add_1, add_2
+
+
+def test_add_1():
+    assert add_1(3) == 4
+
+def test_add_2():
+    assert add_2(2) == 4
