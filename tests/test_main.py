05a2b7d1448035b7ebe74c4594c2358bbc8efdd7
Platon <xnpltn@gmail.com>
2023-12-23 02:24:41 +0200

    configuring tox

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,8 @@ from add.main import add_1, add_2
 
 
 def test_add_1():
-    assert add_1(3) == 4
+    assert add_1(1) == 2
+
 
 def test_add_2():
-    assert add_2(2) == 4
+    assert add_2(1) == 3
